aff99017425f3db07540c2694aef0e4c8a592758
Yuanyf <yuanyf@dongwu-inc.com>
2022-08-08 09:09:46 +0800

    Initialize Project

---
+++ Source/Wildsplanet_Demo/Wildsplanet_DemoGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Wildsplanet_DemoGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WILDSPLANET_DEMO_API AWildsplanet_DemoGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
